a3ed2f8f99c151492fda8dd59a4287b93533896a
Puzzles65 <chitrungnguyen12@gmail.com>
2023-07-24 21:12:22 +0100

    edit

--- controllers/api/items.js
+++ controllers/api/items.js
@@ -11,8 +11,7 @@ module.exports = {
 
 async function index(req, res) {
   const items = await Item.find({}).sort('name').populate('category').exec();
-  // re-sort based upon the sortOrder of the populated categories
-  // items.sort((a, b) => a.category.sortOrder - b.category.sortOrder);
+  
   console.log(items)
   res.json(items);
   
